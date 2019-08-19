4e6daec007ae48042c20092695c28e8db16ff7ff
root <root@localhost.localdomain>
2019-08-19 03:10:43 +0200

    create a print handler void printhandler() function inside the ft_printf function

--- ft_printf.c
+++ ft_printf.c
@@ -14,6 +14,17 @@ static void printxx(char *s, va_list args)
 	(*s == 'o' || *s == 'x' || *s == 'X' || *s == 'u') ? outuint(*s, args, 'a') : 0;
 }
 
+static void printhandler( const char **str, va_list args)
+{
+	int i;
+
+	if ((i = ft_strspn((*str), "dicsoxXufFeEaAgG")))
+		printstr((char)*str[0], args);
+	if ((i = ft_strspn(*str++, "#") || ft_strspn(*str, "oxX")))
+		printxx((char *)(*str), args);
+	*str += i;
+}
+
 int ft_printf(const char *str, ...)
 {
     va_list args;
@@ -24,10 +35,7 @@ int ft_printf(const char *str, ...)
         if (*str == '%')
         {
             str++;
-            if (ft_strspn(&(*str), "dicsoxXufFeEaAgG"))
-                printstr(*str, args);
-	    if (ft_strspn(&(*str++), "#") || ft_strspn(&(*str), "oxX"))
-		printxx(&(*(char *)str), args);
+	    printhandler(&str, args);
         }
         else
             ft_putchar(*str);
